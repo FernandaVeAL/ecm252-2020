0faf1a00e7c06c1a2a55f5643e0418c9555a9e44
Fernanda <fernandaveneroso@hotmail.com>
2020-06-09 15:46:07 -0300

    'Atividade8'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PaginaInicialComponent } from './pages/pagina-inicial/pagina-inicial.component';
+import { ProfessoresComponent } from './pages/professores/professores.component';
+import { SugestoesComponent } from './pages/sugestoes/sugestoes.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'pagina-inicial', component: PaginaInicialComponent },
+  { path: 'professores', component: ProfessoresComponent },
+  { path: 'sugestoes', component: SugestoesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaginaInicialComponent } from './pages/pagina-inicial/pagina-inicial.component';
+import { ProfessoresComponent } from './pages/professores/professores.component';
+import { SugestoesComponent } from './pages/sugestoes/sugestoes.component';
+import { LoginComponent } from './pages/partials/login/login.component';
+import { ListaCursosComponent } from './pages/partials/lista-cursos/lista-cursos.component';
+import { ListaProfessoresComponent } from './pages/partials/lista-professores/lista-professores.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginaInicialComponent,
+    ProfessoresComponent,
+    SugestoesComponent,
+    LoginComponent,
+    ListaCursosComponent,
+    ListaProfessoresComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
